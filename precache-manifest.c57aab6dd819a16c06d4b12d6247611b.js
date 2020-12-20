40888b600321cf719b7d8740700487d859a9e8a4
Akshat Bansal <akshat2610@gmail.com>
2020-12-20 01:13:32 -0800

    Updates

---
+++ precache-manifest.c57aab6dd819a16c06d4b12d6247611b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dad73cc25c3a72f624c304012fce127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "455a3fde2fc55899d6d2",
+    "url": "/static/css/main.48ca62e3.chunk.css"
+  },
+  {
+    "revision": "c5a29219c05c9a9bd8c5",
+    "url": "/static/js/2.3f69e342.chunk.js"
+  },
+  {
+    "revision": "455a3fde2fc55899d6d2",
+    "url": "/static/js/main.6249fc60.chunk.js"
+  },
+  {
+    "revision": "9f0496347e514a7bab67",
+    "url": "/static/js/runtime-main.914512f5.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0a4888f8594716d6e374f9b2dce780bd",
+    "url": "/static/media/SNLogo.0a4888f8.png"
+  },
+  {
+    "revision": "8ae3eb37e8e069db9f2386678998bcaf",
+    "url": "/static/media/handshakeLogo.8ae3eb37.png"
+  },
+  {
+    "revision": "9d633c7d6941867195d7fb7eb9bd21b7",
+    "url": "/static/media/linkedinLogo.9d633c7d.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "b9ad69a43b5bec4129bfccaf9ac0c89b",
+    "url": "/static/media/profilePhoto.b9ad69a4.jpeg"
+  },
+  {
+    "revision": "f40d2ccd92a62a10f5a076a1e324ba13",
+    "url": "/static/media/rocketLogo.f40d2ccd.png"
+  },
+  {
+    "revision": "720b1cbe0644a0ee142d0adc12989cd6",
+    "url": "/static/media/sjsuLogo.720b1cbe.png"
+  },
+  {
+    "revision": "25b7d1434e63c9f06c3bd8250e5f44e3",
+    "url": "/static/media/systemLogo.25b7d143.png"
+  },
+  {
+    "revision": "7e31b8929b6cbbf7a53b6274e77fa836",
+    "url": "/static/media/valuesLogo.7e31b892.png"
+  }
+]);
