a1c331da6e024d4c21c0107e083ad6d342edc8dc
Akshat Bansal <akshat2610.dev@gmail.com>
2022-06-15 14:08:38 -0700

    Updates

---
+++ precache-manifest.8cc7bf2328920986403b5c0e3927c6f4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a211b1aeebf5f06c01db831b9f13d9cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01a0f3684052afbae9d4",
+    "url": "/static/css/main.6363e689.chunk.css"
+  },
+  {
+    "revision": "36e9810408b12b36d7e5",
+    "url": "/static/js/2.c9a6d65d.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/static/js/2.c9a6d65d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a0f3684052afbae9d4",
+    "url": "/static/js/main.8b980276.chunk.js"
+  },
+  {
+    "revision": "727bb81301c0a0813296",
+    "url": "/static/js/runtime-main.813b8e1e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "895055e58873480949ef2c06f8daf8bf",
+    "url": "/static/media/SDLCLogo.895055e5.png"
+  },
+  {
+    "revision": "efadadf6a9f4597fa58e1b6001680dd5",
+    "url": "/static/media/SNLogo.efadadf6.png"
+  },
+  {
+    "revision": "c6e65d793756fdfc3e624c0d8294b3de",
+    "url": "/static/media/linkedinLogo.c6e65d79.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "807aa889c45f4123104c6e736523cd68",
+    "url": "/static/media/profilePhoto.807aa889.jpeg"
+  },
+  {
+    "revision": "67f3e18f62e8292ff3dc101e13f55c9c",
+    "url": "/static/media/rocketLogo.67f3e18f.png"
+  },
+  {
+    "revision": "8031c795c922e5738963e1dcf6f9bd5f",
+    "url": "/static/media/sjsuLogo.8031c795.png"
+  },
+  {
+    "revision": "25b7d1434e63c9f06c3bd8250e5f44e3",
+    "url": "/static/media/systemLogo.25b7d143.png"
+  },
+  {
+    "revision": "7e31b8929b6cbbf7a53b6274e77fa836",
+    "url": "/static/media/valuesLogo.7e31b892.png"
+  }
+]);
